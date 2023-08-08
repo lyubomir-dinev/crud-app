388cf09f848b020ed717ba5d8d13e58ad985f96f
Lyubomir Dinev <dinev.lyubomir@gmail.com>
2023-08-08 13:30:01 +0100

    clean up

--- cypress/e2e/e2e-2.cy.js
+++
@@ -1,9 +0,0 @@
-describe("e2e-2", () => {
-  it("tests e2e-2", () => {
-    cy.viewport(2560, 1245);
-    cy.visit("http://localhost:3000/");
-    cy.get("tbody button").click();
-    cy.get("button.bg-primary").click();
-  });
-});
-//# recorderSourceMap=BCBDBEBFB
